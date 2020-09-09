5fa643e7ce5e35d9efb7823436f54462f560fc20
Emily Bartman <frank@emwaves.org>
2020-09-08 21:33:04 -0700

    migrations working

--- knexfile.js
+++ knexfile.js
@@ -18,5 +18,15 @@ module.exports = {
   },
   production: {
     ...postgres,
+    pool: {
+      min: 2,
+      max: 6,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+      propagateCreateError: false, // <- default is true, set to false
+    },
   },
 }
